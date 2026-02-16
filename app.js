1857d672827d8f719cd1f0c0baa3ec3c22b66083
+ <+>
2026-02-16 13:02:06 +0300

    Added Eldoret sample data

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const places = [
     new Place("Nairobi", "Nairobi National Park", "Summer", "Saw lions and giraffes"),
     new Place("Mombasa", "Fort Jesus", "Winter", "Loved the coastal vibe"),
-    new Place("Kisumu", "Lake Victoria", "Spring", "Enjoyed the sunset by the lake")
+    new Place("Kisumu", "Lake Victoria", "Spring", "Enjoyed the sunset by the lake"),
+    new Place("Eldoret", "Chepkit Waterfalls", "Spring", "Enjoyed the waterfall with lush surroundngs"),
   ];
   
-  const list = document.getElementById("places-list");
+  const list = document.getElementById("places-list")
   const details = document.getElementById("place-details");
   
   places.forEach((place, index) => {
